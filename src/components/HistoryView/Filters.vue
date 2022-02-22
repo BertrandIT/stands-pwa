578cffc01b812a7fd7c78562fc92a2288f2cf8cf
Willo0ow <dhinc39@gmail.com>
2022-02-22 19:02:02 +0100

    filters for logs

---
+++ src/components/HistoryView/Filters.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-text-field
+          label="Data początkowa"
+          type="date"
+          v-model="startDate"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+          label="Data Końcowa"
+          type="date"
+          v-model="endDate"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-select
+          :items="statuses"
+          label="Status stojaka"
+          v-model="selected.status"
+          item-text="text"
+          item-value="value"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          :items="localizations"
+          label="Lokalizacja"
+          v-model="selected.localization"
+          item-text="text"
+          item-value="value"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          :items="deadlines"
+          label="Deadline"
+          v-model="selected.deadline"
+          item-text="text"
+          item-value="value"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-text-field label="Nr stojaka" v-model="standBarcode"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field label="Klient" v-model="client"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn width="100%" color="primary" @click="filterStands"
+          >Filtruj</v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  props: { sendFiltersToParent: { type: Function, required: true } },
+  data() {
+    return {
+      startDate: null,
+      endDate: null,
+      client: "",
+      standBarcode: "",
+      selected: {
+        deadline: null,
+        status: null,
+        localization: null,
+      },
+      statuses: [
+        { text: "Wszystkie", value: null },
+        { text: "Zwrócony", value: "Zwrócony" },
+        { text: "Gotowy do wysyłki", value: "Gotowy do wysyłki" },
+        { text: "Wysłany", value: "Wysłany" },
+        { text: "Zmiana lokalizacji", value: "Zmiana lokalizacji" },
+        { text: "Przyjęcie na stan", value: "Przyjęcie na stan" },
+      ],
+      localizations: [
+        { text: "Wszystkie", value: null },
+        { text: "PVC", value: "B_PVC" },
+        { text: "ALU", value: "B_ALU" },
+        { text: "DRE", value: "B_DRE" },
+      ],
+      deadlines: [
+        { text: "Wszystkie", value: null },
+        { text: "Zbliżający się termin", value: 1 },
+        { text: "Po terminie", value: 2 },
+      ],
+    };
+  },
+  methods: {
+    filterStands() {
+      this.sendFiltersToParent({
+        ...this.selected,
+        startDate: this.startDate,
+        endDate: this.endDate,
+        client: this.client,
+        standBarcode: this.standBarcode,
+      });
+    },
+  },
+};
+</script>
