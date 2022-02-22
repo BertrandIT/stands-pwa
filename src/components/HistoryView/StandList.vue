948aff212c264f654b963553c3f8d831390f6df5
Willo0ow <dhinc39@gmail.com>
2022-02-22 19:02:31 +0100

    basic retrieved stand list

---
+++ src/components/HistoryView/StandList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-virtual-scroll
+    width="100%"
+    :items="stands"
+    item-height="50"
+    height="310px"
+  >
+    <template v-slot="{ item }">
+      <v-list-item>
+        <v-list-item-content>{{ item.barcode }}</v-list-item-content>
+      </v-list-item>
+    </template>
+  </v-virtual-scroll>
+</template>
+<script>
+export default {
+  props: { stands: { type: Array, required: true } },
+  data() {
+    return {};
+  },
+};
+</script>
