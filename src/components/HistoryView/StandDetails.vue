575355679e616891aa0a0e246c2f39301fe5efe3
Willo0ow <dhinc39@gmail.com>
2022-02-22 19:03:29 +0100

    history view structure, retrieve stands

---
+++ src/components/HistoryView/StandDetails.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Stand Details</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>Logi</h1>
+  <div class="history-view">
+    <filters :sendFiltersToParent="getFilters"></filters>
+    <stand-list :stands="stands"></stand-list>
   </div>
 </template>
+<script>
+import Filters from "@/components/HistoryView/Filters.vue";
+import StandList from "@/components/HistoryView/StandList.vue";
+import axios from "@/axios";
+
+export default {
+  components: { Filters, StandList },
+  data() {
+    return {
+      stands: [],
+      filters: {},
+    };
+  },
+  async created() {
+    await this.getStands();
+  },
+  methods: {
+    async getStands() {
+      // const res = await axios.get(
+      //   `/standsHistoryPwa?startDate=${startDate}&endDate=${endDate}&localization=${localization}&action=${status}&client=${client}&deadline=${deadline}&standBarcode=${
+      //     standBarcode ? standBarcode.trim() : standBarcode
+      //   }`
+      // );
+      const res = await axios.get("/standsHistoryPwa");
+      this.stands = res.data;
+    },
+    getFilters(filters) {
+      this.filters = { ...filters };
+    },
+  },
+};
+</script>
