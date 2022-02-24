be5b00ce544c5297d12c859d99d041ca201ba143
zzytny <zuzanna.zytny@bertrand.pl>
2022-02-24 08:53:45 +0100

    test filtruj

---
+++ test/libHomeView/libHistoryView.js
@@ -0,0 +1,12 @@
+class Biblio{
+    innerText;
+    get button () {return $$('//button[text()="'+this.innerText+'"]')}
+
+    clickButton(innerText){
+        this.innerText = innerText;
+        this.button.click();
+    }
+
+}
+
+ module.exports = new Biblio();
