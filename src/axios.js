48211106e2ad676dfb1705a80d258fb5370d2846
zzytny <zuzanna.zytny@bertrand.pl>
2022-02-25 08:16:18 +0100

    change axios

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // axios.defaults.baseURL = "http://192.168.1.4/api";
 //lokalny development (w BBS lokalnie php artisan serve)
 <<<<<<< HEAD
+<<<<<<< HEAD
 // axios.defaults.baseURL = "http://127.0.0.1:8000/";
 <<<<<<< HEAD
 axios.defaults.baseURL = "http://192.168.0.101/projekty/dataviewer2/public/";
@@ -13,4 +14,8 @@ axios.defaults.baseURL = "http://127.0.0.1:8000/";
 // axios.defaults.baseURL = "https://192.168.1.10/dataviewer2/public/";
 >>>>>>> 19f250f (test filtruj)
 >>>>>>> e909ccc (test filtruj)
+=======
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+// axios.defaults.baseURL = "https://192.168.1.10/dataviewer2/public/";
+>>>>>>> 484a691 (change axios)
 export default axios;
