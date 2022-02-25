1f5a689a5272120c5b5d19f15ebb256a6e57e26d
Mateusz Wicki <mateuszwicki1@gmail.com>
2022-02-25 13:33:55 +0100

    login

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,10 +1,30 @@
 <template>
   <v-app-bar app color="primary" dark>
     <router-link to="/"><v-btn text>Stojaki</v-btn></router-link>
-
+    <router-link v-if="user.email" to="/"
+      ><v-btn text @click="logout"
+        ><v-icon>mdi-account</v-icon>
+        <p class="mt-4">{{ user.email }}</p></v-btn
+      ></router-link
+    >
     <v-spacer></v-spacer>
-    <v-btn text>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
+    <v-btn text><v-icon>mdi-open-in-new</v-icon> </v-btn>
   </v-app-bar>
 </template>
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      user: (state) => state.user,
+    }),
+  },
+  methods: {
+    ...mapActions(["loginUser"]),
+    logout() {
+      this.loginUser({});
+    },
+  },
+};
+</script>
