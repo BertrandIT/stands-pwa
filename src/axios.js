0c0b9ab0f97457af762f6e0e3eb80289bafed582
zzytny <zuzanna.zytny@bertrand.pl>
2022-02-25 08:16:18 +0100

    change axios

--- src/axios.js
+++ src/axios.js
@@ -8,5 +8,4 @@ import axios from "axios";
 // axios.defaults.baseURL = "http://192.168.0.101/projekty/dataviewer2/public/";
 // axios.defaults.baseURL = "http://192.168.1.100/projekty/dataviewer2/public/";
 axios.defaults.baseURL = "http://127.0.0.1:8000/";
-// axios.defaults.baseURL = "https://192.168.1.10/dataviewer2/public/";
 export default axios;
